fa3b81df8d1acc86c5bc9efd4df7732b3981a9f5
tmohammad78 <mohammadtaheri506@gmail.com>
2022-04-08 17:52:05 +0430

    add handler

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,14 @@ const std = @import("std");
 const net  = std.net;
 const StreamServer = net.StreamServer;
 const Address = net.Address;
+const GeneralPurposeAllocator = std.heap.GeneralPurposeAllocator;
 pub const io_mode = .evented; // For Async
+const print = std.debug.print;
 
 pub fn main() anyerror!void {
+    var gpa = GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
     var stream_server = StreamServer.init(.{});
     defer stream_server.close();
     const address = try Address.resolveIp("127.0.0.1",8080);
@@ -12,7 +17,31 @@ pub fn main() anyerror!void {
 
     while(true){
         const connection = try stream_server.accept();
-        try connection.stream.writer().print("Hello World", .{} );
-        connection.stream.close();
+        try handler(allocator , connection.stream);
     }
+}
+
+fn handler(allocator: std.mem.Allocator ,stream: net.Stream) !void{
+    _ = allocator;
+    defer stream.close();
+    var first_line = try stream.reader().readUntilDelimiterAlloc(allocator,'\n',std.math.maxInt(usize));
+    first_line = first_line[0..first_line.len];
+    var first_line_iter = std.mem.split(u8,first_line," ");
+
+    const method = first_line_iter.next().?;
+    const uri = first_line_iter.next().?;
+    const version = first_line_iter.next().?;
+
+    var headers = std.StringHashMap([]const u8).init(allocator);
+    while(true){
+        var line = try stream.reader().readUntilDelimiterAlloc(allocator,'\n',std.math.maxInt(usize));
+        if(line.len == 1 and std.mem.eql(u8,line,"\r")) break;
+        line  = line[0..line.len];
+        var line_iter = std.mem.split(u8,line,":");
+        const key = line_iter.next().?;
+        var value = line_iter.next().?;
+        if(value[0] == ' ') value = value[1..];
+        try headers.put(key,value);
+    }
+    print("method: {s}\nuri: {s}\nversion: {s}\n", .{method,uri,version});
 }
