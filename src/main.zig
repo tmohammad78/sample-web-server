ea4254a265852c666298134ad55b371b593a4728
tmohammad78 <mohammadtaheri506@gmail.com>
2022-04-09 23:26:09 +0430

    create struct and fix sth

--- src/main.zig
+++ src/main.zig
@@ -21,27 +21,96 @@ pub fn main() anyerror!void {
     }
 }
 
-fn handler(allocator: std.mem.Allocator ,stream: net.Stream) !void{
-    _ = allocator;
-    defer stream.close();
-    var first_line = try stream.reader().readUntilDelimiterAlloc(allocator,'\n',std.math.maxInt(usize));
-    first_line = first_line[0..first_line.len];
-    var first_line_iter = std.mem.split(u8,first_line," ");
-
-    const method = first_line_iter.next().?;
-    const uri = first_line_iter.next().?;
-    const version = first_line_iter.next().?;
-
-    var headers = std.StringHashMap([]const u8).init(allocator);
-    while(true){
-        var line = try stream.reader().readUntilDelimiterAlloc(allocator,'\n',std.math.maxInt(usize));
-        if(line.len == 1 and std.mem.eql(u8,line,"\r")) break;
-        line  = line[0..line.len];
-        var line_iter = std.mem.split(u8,line,":");
-        const key = line_iter.next().?;
-        var value = line_iter.next().?;
-        if(value[0] == ' ') value = value[1..];
-        try headers.put(key,value);
+const ParsingError = error {
+    MethodNotValid,
+    VersionNotValid
+};
+const Method = enum {
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    OPTION,
+    DELETE,
+    pub fn formString(s: []const u8) !Method {
+        return switch (true) {
+            std.mem.eql(u8,"GET",s) => .GET,
+            std.mem.eql(u8,"POST",s) => .POST,
+            std.mem.eql(u8,"PUT",s) => .PUT,
+            std.mem.eql(u8,"PATCH",s) => .PATCH,
+            std.mem.eql(u8,"OPTION",s) => .OPTION,
+            std.mem.eql(u8,"DELETE",s) => .DELETE,
+            else => ParsingError.MethodNotValid,
+        };
     }
-    print("method: {s}\nuri: {s}\nversion: {s}\n", .{method,uri,version});
+};
+
+const Version = enum {
+    @"1.1",
+    @"2",
+    pub fn formString(s: []const u8) !Version{
+        return switch (true) {
+            std.mem.eql(u8,"HTTP/1.1",s) => .@"1.1",
+            std.mem.eql(u8,"HTTP/2",s) => .@"2",
+            else => ParsingError.VersionNotValid
+        };
+    }
+};
+const HTTPContext = struct {
+    method: Method,
+    uri: []const u8,
+    version: Version,
+    headers: std.StringHashMap([]const u8),
+    stream: net.Stream,
+
+    pub fn body(self: *HTTPContext) net.Sream.Reader {
+        return self.stream.reader();
+    }
+
+    pub fn response(self: *HTTPContext) net.Stream.writer {
+        return self.stream.writer();
+    }
+
+    pub fn debugPrintRequest(self: *HTTPContext) void {
+        print("method: {s}\nuri: {s}\nversion: {s}\n", .{self.method,self.uri,self.version});
+        var header_itter = self.headers.iterator();
+        while (header_itter.next()) |entry| {
+            print("{s}: {s}\n", .{entry.key_ptr.*,entry.value_ptr.*});
+        }
+    }
+
+    pub fn init(allocator: std.mem.Allocator,stream: net.Stream) !HTTPContext {
+        var first_line = try stream.reader().readUntilDelimiterAlloc(allocator,'\n',std.math.maxInt(usize));
+        first_line = first_line[0..first_line.len];
+        var first_line_iter = std.mem.split(u8,first_line," ");
+
+        const method = first_line_iter.next().?;
+        const uri = first_line_iter.next().?;
+        const version = first_line_iter.next().?;
+
+        var headers = std.StringHashMap([]const u8).init(allocator);
+        while(true){
+            var line = try stream.reader().readUntilDelimiterAlloc(allocator,'\n',std.math.maxInt(usize));
+            if(line.len == 1 and std.mem.eql(u8,line,"\r")) break;
+            line  = line[0..line.len];
+            var line_iter = std.mem.split(u8,line,":");
+            const key = line_iter.next().?;
+            var value = line_iter.next().?;
+            if(value[0] == ' ') value = value[1..];
+            try headers.put(key,value);
+        }
+
+        return HTTPContext{
+            .headers= headers,
+            .method = try Method.formString(method),
+            .version = try Version.formString(version),
+            .uri = uri,
+            .stream = stream,
+        };
+    }
+};
+fn handler(allocator: std.mem.Allocator ,stream: net.Stream) !void{
+    defer stream.close();
+    var http_context  = try HTTPContext.init(allocator,stream);
+    http_context.debugPrintRequest();
 }
